cf3b6aaffd15193e777807f3e3d061031de95dc6
hellonayeon <hellonykwon@gmail.com>
2022-11-01 02:05:47 +0900

    Chore: AWS Secret Manager 연동(미해결), 관련 의존성 추가 및 yml 파일 작성 #KOK-88

--- src/main/java/shop/kokodo/configservice/ConfigServiceApplication.java
+++ src/main/java/shop/kokodo/configservice/ConfigServiceApplication.java
@@ -1,5 +1,7 @@
 package shop.kokodo.configservice;
 
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +9,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
+	@Value("${encrypt.key-store.secret}")
+	private String test;
+
+	@PostConstruct
+	public void test() {
+		System.out.println(">>> " + test);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
